6f60bfd6c1d4c50df0ff1ab8a31e57e72bb8aa15
qwertyyb <qwertyyb@qq.com>
2019-09-15 15:40:45 +0800

    Initial Commit

---
+++ Fire/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Fire
+//
+//  Created by 虚幻 on 2019/9/15.
+//  Copyright © 2019 qwertyyb. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
