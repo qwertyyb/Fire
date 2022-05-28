f35635266bf2fe9379f78dd536c38589ad1847f9
qwertyyb <qwertyyb@foxmail.com>
2022-05-28 22:38:43 +0800

    fix: 优化安装体验 (#57)
    * feat: 统计输入
    
    * feat: 关闭首选项窗口之后,释放内存
    
    * feat: 添加清除统计数据的能力,可选择启用是否启用数据统计
    
    * fix: 移除sqlite.swift依赖
    
    * fix: 修复统计设置不生效的问题
    
    * feat: 添加数据库迁移能力
    
    * fix: 移除过时的依赖项
    
    * feat: 使用sqlcipher加密数据库
    
    * fix: 优化安装流程
    
    * fix: 修复安装不结束的问题
    
    Co-authored-by: qwertyyb <qwertyyb@qq.com>

--- Fire/AppDelegate.swift
+++ Fire/AppDelegate.swift
@@ -17,28 +17,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func installInputSource() {
         print("install input source")
+        InputSource.shared.deactivateInputSource()
         InputSource.shared.registerInputSource()
         InputSource.shared.activateInputSource()
+    }
+
+    func stop() {
+        InputSource.shared.deactivateInputSource()
         NSApp.terminate(nil)
     }
 
-    private func commandHandler() {
+    private func commandHandler() -> Bool {
         if CommandLine.arguments.count > 1 {
             print("[Fire] launch argument: \(CommandLine.arguments[1])")
             let command = CommandLine.arguments[1]
             if command == "--install" {
-                return installInputSource()
+                installInputSource()
+                return false
             }
             if command == "--build-dict" {
                 print("[Fire] build dict")
                 buildDict()
-                return NSApp.terminate(nil)
+                NSApp.terminate(nil)
+                return false
+            }
+            if command == "--stop" {
+                print("[Fire] stop")
+                stop()
+                return false
             }
         }
+        return true
     }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        commandHandler()
+        if !commandHandler() {
+            return
+        }
         if !hasDict() {
             NSLog("[Fire] first run，build dict")
             buildDict()
