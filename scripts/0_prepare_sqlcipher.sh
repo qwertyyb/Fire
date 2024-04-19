7f0c7cce94fc88ad966755a3bfac09da4778e9ad
qwertyyb <qwertyyb@foxmail.com>
2024-04-19 14:31:35 +0800

    chore: 从TableBuilder 中移除 SQLCipher

--- scripts/0_prepare_sqlcipher.sh
+++ scripts/0_prepare_sqlcipher.sh
@@ -3,15 +3,20 @@
 PROJECT_ROOT="$(cd "$(dirname "$BASH_SOURCE")/.."; pwd)"
 
 SQLCIPHER="$PROJECT_ROOT/sqlcipher"
-DIST_DIR="$PROJECT_ROOT/Fire/SQLCipher"
 
 cd $SQLCIPHER
 ./configure --with-crypto-lib=none
 make sqlite3.c
 
-echo "构建完成，移动 sqlite3.c 和 sqlite3.h 到 Fire/SQLCipher/ 目录"
-mv -f sqlite3.c "$DIST_DIR/sqlite3.c"
-mv -f sqlite3.h "$DIST_DIR/sqlite3.h"
+echo "构建完成，复制 sqlite3.c 和 sqlite3.h 到 Fire/SQLCipher/ 目录"
+DIST_DIR="$PROJECT_ROOT/Fire/SQLCipher"
+cp -f sqlite3.c "$DIST_DIR/sqlite3.c"
+cp -f sqlite3.h "$DIST_DIR/sqlite3.h"
+
+echo "复制 sqlite3.c 和 sqlite3.h 到 TableBuilder/SQLCipher/ 目录"
+DIST_DIR="$PROJECT_ROOT/TableBuilder/SQLCipher"
+cp -f sqlite3.c "$DIST_DIR/sqlite3.c"
+cp -f sqlite3.h "$DIST_DIR/sqlite3.h"
 
 echo "清理仓库"
 git reset --hard
