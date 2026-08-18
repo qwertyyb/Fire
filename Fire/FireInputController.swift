c17afc77910a5822c107d1bd009894c8b4d5cfbc
qwertyyb <qwertyyb@foxmail.com>
2026-08-18 14:10:40 +0800

    fix: 修复输入框中首个字符是数字，后输入的句号不会转为小数点的问题。fix #196

--- Fire/FireInputController.swift
+++ Fire/FireInputController.swift
@@ -51,7 +51,8 @@ class FireInputController: IMKInputController, InputContext  {
             // selectedRange的location在组字区前
             previousLocation = selectedRange.location - 1
         }
-        if previousLocation <= 0 {
+        FireLog.input.debug("getTextBefore, markedRange: \(markedRange), selectedRange: \(selectedRange), previousLocation: \(previousLocation)")
+        if previousLocation < 0 {
             return ""
         }
         return client().attributedSubstring(from: NSMakeRange(previousLocation, 1))?.string ?? ""
